51a3e386b83ff9ec175fe3277be22b17ed14be45
Juan Miguel <oliveirajuan243@gmail.com>
2026-02-20 21:17:04 -0300

    repository finalizado

--- repository/FlahcardRepository.py
+++ repository/FlahcardRepository.py
@@ -5,6 +5,17 @@ class FlashcardRepository:
     def save (driver, categoria, tipo, flashcard, usuario):
         driver.execute_query(
             """
-            MERRGE (c:categoria{categoria $categoria})
-            """
+            MERGE (c:categoria{categoria:$categoria})
+            MERGE (t:tipo {tipo:$tipo})
+            MERGE (f:flashcard {flashcard:$flashcard})
+            MERGE (u:usuario {usuario:$usuario})
+
+            MERGE (c)-[:CATEGORIA]->(t)
+            MERGE (t)-[:TIPO_do_flash_card]->(f)
+            MERGE (f)-[:Criado_por]->(u)
+            """,
+            categoria=categoria,
+            tipo=tipo,
+            flashcard=flashcard,
+            usuario=usuario
         )
